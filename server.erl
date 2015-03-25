3902cb29bf63e9ef30c5ed8d45be4ed6792a9790
Lukas Klingsbo <lukas.klingsbo@gmail.com>
2015-03-25 03:50:26 +0100

    Added lock logic

--- server.erl
+++ server.erl
@@ -79,7 +79,7 @@ server_loop(ClientList, StorePid, TransState) ->
                         1 -> 
                             server_loop(ClientList, StorePid, add_action(Client, Act, Num, TransState));
                         _ ->
-                            Client ! {abort,self()},
+                            Client ! {abort, self()},
                             io:format("Lost msg detected.~n"),
                             server_loop(ClientList, StorePid, delete_actions(Client, TransState))
                     end;
@@ -88,7 +88,7 @@ server_loop(ClientList, StorePid, TransState) ->
                         Num ->
                             server_loop(ClientList, StorePid, add_action(Client, Act, Num, TransState));
                         _ ->
-                            Client ! {abort,self()},
+                            Client ! {abort, self()},
                             io:format("Lost msg detected.~n"),
                             server_loop(ClientList, StorePid, delete_actions(Client, TransState))
                     end
@@ -99,9 +99,54 @@ server_loop(ClientList, StorePid, TransState) ->
             end
     end.
 
-%% - The logic around the locks are maintained here
-transaction(ClientPid, Transaction) ->
-    [].
+%% - The logic around executing the transaction is done here
+%%   TODO: How does one operation of a transaction look like?
+transaction([], _StorePid) ->
+    something;
+transaction(Transaction, StorePid) ->
+    something.
+
+%% - The values are maintained here
+store_loop(Database, Locks) ->
+    receive
+        {print, _Pid} ->
+            io:format("Database status:~n~p.~n", [Database]),
+            store_loop(Database, Locks);
+        {transaction, Transaction, Pid} -> % TODO: Probably shouldn't have this one as problem gets too simple?
+            LocksNeeded = lock_check(Transaction),
+            case request_locks(LocksNeeded, Pid, Locks) of
+                {true, ModLocks} ->
+                    Pid ! transaction(Transaction, self());
+                _ ->
+                    %% Abort
+                    Pid ! abort
+            end,
+            store_loop(Database, unlock_all(Pid, Locks));
+        {read, Prop, Pid} ->
+            Pid ! read(Prop, Database),
+            store_loop(Database, Locks);
+        {write, Prop, Value, _Pid} ->
+            store_loop(write(Prop, Value, Database), Locks);
+        {lock, Prop, LockType, Pid} -> % TODO: Will not work as Lock() is rewritten
+            store_loop(Database, lock({Prop, LockType}, Pid, Locks));
+        {unlock, Prop, LockType, _Pid} -> % TODO: Will not work as Unlock() is rewritten
+            store_loop(Database, unlock({Prop, LockType}, Locks))
+    end.
+
+%%%%%%%%%%%%%%%%%%%%%%% ACTIVE SERVER %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%%%%%% DATA MODIFICATION METHODS %%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% - Checks whether all locks can be obtained or not
+%%   TODO: Should probably call store_loop to lock and unlock as problem is
+%%   trivial otherwise
+request_locks(LocksNeeded, ClientPid, Locks) ->
+    request_locks(LocksNeeded, ClientPid, Locks, true).
+request_locks([], _ClientPid, Locks, Result) ->
+    {Result, Locks};
+request_locks([Lock | TL], ClientPid, Locks, Result) ->
+    {Modified, ModLocks} = lock(Lock, ClientPid, Locks),
+    request_locks(TL, ClientPid, ModLocks, Result and Modified).
 
 %% - Checks which locks that are required for Transaction
 lock_check([]) ->
@@ -110,27 +155,6 @@ lock_check([{read, Prop} | TL]) ->
     [{Prop, readlock} | lock_check(TL)];
 lock_check([{write, Prop, _Value} | TL]) ->
     [{Prop, writelock} | lock_check(TL)].
-
-%% - The values are maintained here
-store_loop(Database, Locks) ->
-    receive
-        {print, _Pid} ->
-            io:format("Database status:~n~p.~n", [Database]),
-            store_loop(Database, Locks);
-        {read, Prop, Pid} ->
-            Pid ! read(Prop, Database),
-            store_loop(Database, Locks);
-        {write, Prop, Value, _Pid} ->
-            store_loop(write(Prop, Value, Database), Locks);
-        {lock, Prop, LockType, Pid} ->
-            store_loop(Database, lock({Prop, LockType}, Pid, Locks));
-        {unlock, Prop, LockType, _Pid} ->
-            store_loop(Database, unlock({Prop, LockType}, Locks))
-    end.
-
-%%%%%%%%%%%%%%%%%%%%%%% ACTIVE SERVER %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-%%%%%%%%%%%%%%%%%%%%%%% DATA MODIFICATION METHODS %%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %% - Returns the Database with the tuple containing the Prop key
 %%   set to {Prop, Value}
@@ -141,25 +165,37 @@ write(Prop, Value, Database) ->
 read(Prop, Database) ->
     lists:keyfind(Prop, 1, Database).
 
-%% - Returns the Locks list with the Lock tuple set to locked
-lock(Lock, Pid, Locks) -> semaphore(Lock, Pid, Locks).
+%% - Returns the Locks list with true and the Lock tuple set to locked 
+%%   operation succeeded and false and the old Lock tuple if the operation
+%%   failed
+lock(Lock, Pid, Locks) ->
+    Modified = semaphore(Lock, Pid, Locks),
+    {Modified == Locks, Modified}.
 
 %% - Returns the Locks list with the Lock tuple set to unlocked
 unlock(Lock, Locks) -> semaphore(Lock, [], Locks).
 
-%% - Manipulation of lock tuples
+%% - Unlock all locks for a certain Pid
+unlock_all(_Pid, []) ->
+    [];
+unlock_all(Pid, [{Prop, LockType, Pid} | TL]) ->
+    [{Prop, LockType, []} | unlock_all(Pid, TL)].
+
+%% - Manipulation of lock tuples, only to be used by the
+%%   lock and unlock functions
 semaphore(_Lock, _Pid, []) ->
     [];
-semaphore(Lock = {Prop, readlock}, Pid, [{Prop, readlock, _Value} | TL]) ->
-    [{Prop, readlock, Pid} | lock(Lock, Pid, TL)];
-semaphore(Lock = {Prop, writelock}, Pid, [{Prop, LockType, _Value} | TL]) ->
-    [{Prop, LockType, Pid} | lock(Lock, Pid, TL)];
+semaphore(Lock = {Prop, readlock}, Pid, [{Prop, readlock, []} | TL]) ->
+    [{Prop, readlock, Pid} | semaphore(Lock, Pid, TL)];
+semaphore(Lock = {Prop, writelock}, Pid, [{Prop, LockType, []} | TL]) ->
+    [{Prop, LockType, Pid} | semaphore(Lock, Pid, TL)];
 semaphore(Lock, Pid, [HD | TL]) ->
     [HD | semaphore(Lock, Pid, TL)].
 
+%% TODO: These need better variable names
 add_action(_C, _A, _O, []) -> [];
-add_action(C, A, O, [{C, Actions} | T]) -> [{C, [{A,O} | Actions]} | T];
-add_action(C, A, O, [{H, Actions} | T]) -> [{H,Actions} | add_action(C, A, O, T)].
+add_action(C, A, O, [{C, Actions} | T]) -> [{C, [{A, O} | Actions]} | T];
+add_action(C, A, O, [{H, Actions} | T]) -> [{H, Actions} | add_action(C, A, O, T)].
 
 delete_actions(_C, []) -> [];
 delete_actions(C, [{C, _Actions} | T]) -> T;
